f99f66eac7d1680d3e7efa9854105fd50e0b46c0
samael33 <IgorOlhovskiy@gmail.com>
2017-05-23 12:30:54 +0300

    astpp.functions.lua modifications

--- freeswitch/scripts/astpp/lib/astpp.functions.lua
+++ freeswitch/scripts/astpp/lib/astpp.functions.lua
@@ -80,7 +80,7 @@ function check_did(destination_number,config)
 	number_translation = config['did_global_translation'];
 	destination_number = do_number_translation(number_translation,destination_number)
 
-	local query = "SELECT A.id as id,B.id as accountid,B.number as account_code,A.number as  did_number,A.connectcost,A.includedseconds,A.cost,A.inc,A.extensions,A.maxchannels,A.call_type,A.city,A.province,A.init_inc FROM "..TBL_DIDS.." AS A,"..TBL_USERS.." AS B WHERE A.status=0 AND B.status=0 AND B.deleted=0 AND B.id=A.accountid AND A.number =\"" ..destination_number .."\" LIMIT 1";
+	local query = "SELECT A.id as id,B.id as accountid,B.number as account_code,A.number as  did_number,A.connectcost,A.includedseconds,A.cost,A.inc,A.extensions,A.maxchannels,A.call_type,A.city,A.province,A.init_inc FROM "..TBL_DIDS.." AS A,"..TBL_USERS.." AS B WHERE A.status=0 AND B.status=0 AND B.deleted=0 AND B.id=A.accountid AND "..did_fix_query_austrian("A.number", destination_number, 5).." LIMIT 1";
 
 	Logger.debug("[CHECK_DID] Query :" .. query)
 
@@ -96,7 +96,7 @@ function check_did_reseller(destination_number,userinfo,config)
 	number_translation = config['did_global_translation'];
 	destination_number = do_number_translation(number_translation,destination_number)   
 
-	local query = "SELECT A.id as id, A.number AS number,B.cost AS cost,B.connectcost AS connectcost,B.includedseconds AS includedseconds,B.inc AS inc,A.city AS city,A.province,A.call_type,A.extensions AS extensions,A.maxchannels AS maxchannels,A.init_inc FROM "..TBL_DIDS.." AS A,"..TBL_RESELLER_PRICING.." as B WHERE A.number = \"" ..destination_number .."\"  AND B.type = '1' AND B.reseller_id = \"" ..userinfo['reseller_id'].."\" AND B.note =\"" ..destination_number .."\"";
+	local query = "SELECT A.id as id, A.number AS number,B.cost AS cost,B.connectcost AS connectcost,B.includedseconds AS includedseconds,B.inc AS inc,A.city AS city,A.province,A.call_type,A.extensions AS extensions,A.maxchannels AS maxchannels,A.init_inc FROM "..TBL_DIDS.." AS A,"..TBL_RESELLER_PRICING.." as B WHERE "..did_fix_query_austrian("A.number", destination_number, 5).." AND B.type = '1' AND B.reseller_id = \"" ..userinfo['reseller_id'].."\" AND "..did_fix_query_austrian("B.note", destination_number, 5);
 
 	Logger.debug("[CHECK_DID_RESELLER] Query :" .. query)
 	
@@ -526,4 +526,69 @@ function plus_destination_number(destination_number)
 		dnumber = "\\"..dnumber
 	end
     return dnumber
+end
+
+
+-- Modify DID to use in queries to match Austrian rules
+function did_fix_query_austrian(field, dialed_number, offset)
+    local num_length = string.len(dialed_number)
+    if offset >= num_length then
+        offset = num_length - 1
+    end
+    local query_string = "("..field.." = \""..dialed_number.."\""
+    for i = -2, -offset, -1 do
+        query_string = query_string.." OR "..field.." = \""..string.sub(dialed_number, 1, i).."\""
+    end
+    query_string = query_string.." OR "..field.." = \""..string.sub(dialed_number, 1, -offset - 1).."\")"
+
+    return query_string
+end
+
+--- Get CallerID normalization for support of {ani} and {name} keyword
+function normalize_callerid_ani(callerid)
+    if (callerid ~= nil) then
+        callerid_override_name = callerid['cid_name']
+        callerid_override_number = callerid['cid_number']
+        if (callerid_override_name:find('{ani}')) then
+            Logger.debug("[Functions][NORMALIZE_CALLERID_ANI] {ani} in callerid_name found")
+            callerid_name = params:getHeader('Caller-Caller-ID-Name') and params:getHeader('Caller-Caller-ID-Name') or ""
+            callerid_name = callerid_name:match("%d+") and callerid_name:match("%d+") or ""
+            callerid_override_name = callerid_override_name:gsub("{ani}",callerid_name)
+        end
+        if (callerid_override_number:find('{name}')) then
+            Logger.debug("[Functions][NORMALIZE_CALLERID_ANI] {name} in callerid_number found")
+            callerid_override_number = callerid_override_name
+        elseif (callerid_override_number:find('{ani}')) then
+            Logger.debug("[Functions][NORMALIZE_CALLERID_ANI] {ani} in callerid_number found")
+            callerid_number = params:getHeader('Caller-Caller-ID-Number') and params:getHeader('Caller-Caller-ID-Number') or ""
+            callerid_number = callerid_number:match("%d+") and callerid_number:match("%d+") or ""
+            callerid_override_number = callerid_override_number:gsub("{ani}",callerid_number)
+        end
+        if (callerid_override_name:find('{number}')) then
+            Logger.debug("[Functions][NORMALIZE_CALLERID_ANI] {number} in callerid_name found")
+            callerid_override_name = callerid_override_number
+        end
+        Logger.debug("[Functions][NORMALIZE_CALLERID_ANI] CallerID name: "..callerid_override_name..", number: "..callerid_override_number)
+        result = {}
+        result['cid_name'] = callerid_override_name
+        result['cid_number'] = callerid_override_number
+        return result
+    end
+    return nil
+end
+
+
+-- Function to get accountcode for redirected call
+function get_redirected_call_accountcode(did)
+
+    local query = "SELECT number FROM accounts WHERE id = (SELECT accountid FROM dids WHERE "..did_fix_query_austrian("number", did, 5)..") LIMIT 1"
+    Logger.debug("[GET_REDIRECTED_CALL_ACCOUNTCODE] Query :" .. query)
+
+    assert (dbh:query(query, function(u)
+        accountcode = u
+    end))
+    if (accountcode ~= nil) then
+        return accountcode['number']
+    end
+    return nil
 end
