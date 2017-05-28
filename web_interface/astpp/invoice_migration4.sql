729c257020e84a0db2b6281d2f435294f3b3a593
Serg Volodko <s.volodko@entelnet.ru>
2017-05-28 23:01:36 +0300

    Adding general fields 1 - 5 on page account customer.

---
+++ web_interface/astpp/invoice_migration4.sql
@@ -0,0 +1,5 @@
+ALTER TABLE accounts ADD COLUMN general_field1 varchar(255),
+  ADD COLUMN general_field2 varchar(255),
+  ADD COLUMN general_field3 varchar(255),
+  ADD COLUMN general_field4 varchar(255),
+  ADD COLUMN general_field5 varchar(255)
