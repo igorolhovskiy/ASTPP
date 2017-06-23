8d90ba14be07d364837ca5ee3db47e883369a2b7
samael33 <IgorOlhovskiy@gmail.com>
2017-06-23 16:06:42 +0300

    Lua auth redo fix

--- freeswitch/scripts/astpp/lib/astpp.functions.lua
+++ freeswitch/scripts/astpp/lib/astpp.functions.lua
@@ -122,13 +122,13 @@ end
 -- Do Authentication 
 function doauthentication (destination_number,from_ip, sip_authorized, sip_from_user)
 
-    if (sip_authorized == 'true' and sip_from_user ~= nil)
+    if (sip_authorized == 'true' and sip_from_user ~= nil) then
         local query = "SELECT number AS account_code FROM "..TBL_USERS.." WHERE id = (SELECT accountid FROM "..TBL_SIP_DEVICES.." WHERE username = \""..sip_from_user.."\");"
         Logger.debug("[DOAUTHENTICATION] Query :" .. query)
         assert (dbh:query(query, function(u)
             authinfo = u;
         end))
-        if (authinfo ~= nil and authinfo ~= "") 
+        if (authinfo ~= nil and authinfo ~= "") then
             authinfo['name'] = authinfo['account_code'];
             return authinfo
         end
