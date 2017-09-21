05a87c61e5863be6ffb2e83c00a665d9e4f0f4a6
samael33 <IgorOlhovskiy@gmail.com>
2017-09-21 10:03:02 +0300

    Added indexex to tables

--- database/migrations/migrations7.sql
+++ database/migrations/migrations7.sql
@@ -1,1 +1,4 @@
 alter TABLE invoice_details ADD COLUMN count int DEFAULT 0;
+ALTER TABLE invoice_details ADD INDEX(invoiceid), ADD INDEX(item_type);
+ALTER TABLE cdrs ADD INDEX(callstart);
+ALTER TABLE cdrs ADD INDEX(accountid);
