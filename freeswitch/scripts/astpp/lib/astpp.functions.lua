290268bc424809e517e0334f0dbd8baeb2199c61
samael33 <IgorOlhovskiy@gmail.com>
2017-11-14 22:23:51 +0200

    var_dump debug function added

--- freeswitch/scripts/astpp/lib/astpp.functions.lua
+++ freeswitch/scripts/astpp/lib/astpp.functions.lua
@@ -642,3 +642,33 @@ function get_redirected_call_accountcode(did)
     end
     return nil
 end
+
+function var_dump(...)
+    local function string(o)
+        return '"' .. tostring(o) .. '"'
+    end
+    local function recurse(o, indent)
+        if indent == nil then indent = '' end
+        local indent2 = indent .. '  '
+        if type(o) == 'table' then
+            local s = indent .. '{' .. '\n'
+            local first = true
+            for k,v in pairs(o) do
+                if first == false then s = s .. ', \n' end
+                if type(k) ~= 'number' then k = string(k) end
+                s = s .. indent2 .. '[' .. k .. '] = ' .. recurse(v, indent2)
+                first = false
+            end
+            return s .. '\n' .. indent .. '}'
+        else
+            return string(o)
+        end
+    end
+
+    local args = {...}
+    if #args > 1 then
+        var_dump(args)
+    else
+        Logger.debug("[VAR_DUMP]: "..recurse(args[1]))
+    end
+end
