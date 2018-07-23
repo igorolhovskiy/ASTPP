aa7b03682f375546692029afe82cb6315f570fbe
samael33 <IgorOlhovskiy@gmail.com>
2018-07-23 09:20:50 +0300

    Added SQL migrations

--- web_interface/astpp/migration_sofort.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO system
-( name, display_name, value, field_type, comment, timestamp, reseller_id, brand_id, group_title) VALUES
-('sofort_status', 'Sofort', '0', 'enable_disable_option', 'Set enable to add paypal as payment gateway option',
-  NULL, 0, 0, 'sofort');
-INSERT INTO system
-( name, display_name, value, field_type, comment, timestamp, reseller_id, brand_id, group_title) VALUES
-('sofort_configuration_key', 'Sofort Configuration key', '', 'default_system_input', 'Set Sofort Configuration key',
-  NULL, 0, 0, 'sofort');
