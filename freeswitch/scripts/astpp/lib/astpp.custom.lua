69780596ec764f813dbddf8ed61ce7bd0fb70160
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2019-11-04 16:24:02 +0100

    Add freeswitch_xml_outbound override

--- freeswitch/scripts/astpp/lib/astpp.custom.lua
+++ freeswitch/scripts/astpp/lib/astpp.custom.lua
@@ -92,3 +92,108 @@ function check_did_reseller(destination_number,userinfo,config)
 	end))
 	return didinfo;
 end
+
+-- Dialplan for outbound calls OVERRIDE
+function freeswitch_xml_outbound(xml,destination_number,outbound_info,callerid_array,rate_group_id,old_trunk_id,force_outbound_routes,rategroup_type,livecall_data)
+
+	local temp_destination_number = destination_number
+	local tr_localization=nil
+	tr_localization = get_localization(outbound_info['provider_id'],'T')
+
+	
+	if (tr_localization ~= nil) then
+		tr_localization['out_caller_id_terminate'] = tr_localization['out_caller_id_terminate']:gsub(" ", "")
+		-------------- Caller Id translation ---------	 
+		callerid_array['cid_name'] = do_number_translation(tr_localization['out_caller_id_terminate'],callerid_array['cid_name'])
+		callerid_array['cid_number'] = do_number_translation(tr_localization['out_caller_id_terminate'],callerid_array['cid_number'])    
+		xml = freeswitch_xml_callerid(xml,callerid_array)	    	   	    
+    	----------------------------------------------------------------------
+
+    	-------------- Destination number translation ---------
+		tr_localization['number_terminate'] = tr_localization['number_terminate']:gsub(" ", "")
+		temp_destination_number = do_number_translation(tr_localization['number_terminate'],destination_number)
+		-----------------------------------
+		
+	end
+
+	if(outbound_info['prepend'] ~= '' or outbound_info['strip'] ~= '') then
+
+        if (outbound_info['prepend'] == '') then 
+            outbound_info['prepend'] = '*'                        
+        end
+
+        if (outbound_info['strip'] == '') then 
+            outbound_info['strip'] = '*'
+        end
+
+		temp_destination_number = do_number_translation(outbound_info['strip'].."/"..outbound_info['prepend'],temp_destination_number)
+	end
+    
+	xml_termiantion_rates= "ID:"..outbound_info['outbound_route_id'].."|CODE:"..outbound_info['pattern'].."|DESTINATION:"..outbound_info['comment'].."|CONNECTIONCOST:"..outbound_info['connectcost'].."|INCLUDEDSECONDS:"..outbound_info['includedseconds'].."|COST:"..outbound_info['cost'].."|INC:"..outbound_info['inc'].."|INITIALBLOCK:"..outbound_info['init_inc'].."|TRUNK:"..outbound_info['trunk_id'].."|PROVIDER:"..outbound_info['provider_id'];
+
+    table.insert(xml, [[<action application="set" data="calltype=STANDARD"/>]]);        
+	table.insert(xml, [[<action application="set" data="termination_rates=]]..xml_termiantion_rates..[["/>]]);        
+	table.insert(xml, [[<action application="set" data="trunk_id=]]..outbound_info['trunk_id']..[["/>]]);        
+	table.insert(xml, [[<action application="set" data="provider_id=]]..outbound_info['provider_id']..[["/>]]);           
+	table.insert(xml, [[<action application="set" data="rate_flag=]]..rategroup_type..[["/>]]);           
+	table.insert(xml, [[<action application="set" data="force_trunk_flag=]]..force_outbound_routes..[["/>]]);    
+    table.insert(xml, [[<action application="export" data="presence_data=trunk_id=]]..outbound_info['trunk_id']..[["/>]])
+    table.insert(xml, [[<action application="set" data="intcall=]]..(outbound_info['intcall'] and 1 or 0)..[["/>]])
+
+	-- Check if is there any gateway configuration params available for it.
+	if (outbound_info['dialplan_variable'] ~= '') then 
+		Logger.info(" ".. outbound_info['dialplan_variable']);
+		local dialplan_variable = split(outbound_info['dialplan_variable'],",")      
+		for dialplan_variable_key,dialplan_variable_value in pairs(dialplan_variable) do
+			local dialplan_variable_data = split(dialplan_variable_value,"=")  
+			Logger.debug("[GATEWAY VARIABLE ] : "..dialplan_variable_data[1] )
+            if( dialplan_variable_data[1] ~= nil and dialplan_variable_data[2] ~= nil) then
+                if (dialplan_variable_data[1] == 'force_callback') then
+                    callback_function_name = dialplan_variable_data[2]
+                    Logger.debug("[GATEWAY VARIABLE] : Callback triggered to "..callback_function_name)
+                    if (_G[callback_function_name] ~= nil) then
+                        xml, temp_destination_number = _G[callback_function_name](xml, temp_destination_number, callerid_array)
+                    end
+                end
+                table.insert(xml, [[<action application="set" data="]]..dialplan_variable_data[1].."="..dialplan_variable_data[2]..[["/>]])
+			end
+		end             
+	end
+	----------------------- END Gateway configuraiton -------------------------------
+	-- Set force codec if configured
+	
+	--~ livecall_data = livecall_data.."|||"..outbound_info['trunk_name'].." // "..outbound_info['pattern'].." // "..outbound_info['comment'].." // "..outbound_info['cost']
+	
+	if(outbound_info['trunk_id']~=nil) then
+		livecall_data = livecall_data.."|||"..outbound_info['trunk_name'].." // "..outbound_info['pattern'].." // "..outbound_info['comment'].." // "..outbound_info['cost'].." // trunk_id="..outbound_info['trunk_id']
+	else
+		livecall_data = livecall_data.."|||"..outbound_info['trunk_name'].." // "..outbound_info['pattern'].." // "..outbound_info['comment'].." // "..outbound_info['cost']
+	end
+	
+    table.insert(xml, [[<action application="export" data="presence_data=]]..livecall_data..[[|||STD"/>]])
+	
+    chan_var = "leg_timeout="..outbound_info['leg_timeout']
+    if (outbound_info['codec'] ~= '') then
+            chan_var = chan_var..",absolute_codec_string=".."^^:"..outbound_info['codec']:gsub("%,", ":")
+    end            
+
+	-- Set CPS limit for user if > 0
+	if (tonumber(outbound_info['cps']) ~=nil and tonumber(outbound_info['cps']) > 0) then
+		table.insert(xml, [[<action application="limit" data="hash CPS_]]..outbound_info['trunk_id']..[[ CPS_trunk_]]..outbound_info['trunk_id']..[[ ]]..outbound_info['cps']..[[/1 !SWITCH_CONGESTION"/>]]);
+	end
+
+	if(tonumber(outbound_info['maxchannels']) > 0) then    
+		table.insert(xml, [[<action application="limit_execute" data="db ]]..outbound_info['path']..[[ gw_]]..outbound_info['path']..[[ ]]..outbound_info['maxchannels']..[[ bridge []]..chan_var..[[]sofia/gateway/]]..outbound_info['path']..[[/]]..temp_destination_number..[["/>]]);
+	else
+		table.insert(xml, [[<action application="bridge" data="[]]..chan_var..[[]sofia/gateway/]]..outbound_info['path']..[[/]]..temp_destination_number..[["/>]]);
+	end
+
+	if(outbound_info['path1'] ~= '' and outbound_info['path1'] ~= outbound_info['path']) then
+		table.insert(xml, [[<action application="bridge" data="[]]..chan_var..[[]sofia/gateway/]]..outbound_info['path1']..[[/]]..temp_destination_number..[["/>]]);
+	end
+
+	if(outbound_info['path2'] ~= '' and outbound_info['path2'] ~= outbound_info['path'] and outbound_info['path2'] ~= outbound_info['path1']) then
+		table.insert(xml, [[<action application="bridge" data="[]]..chan_var..[[]sofia/gateway/]]..outbound_info['path2']..[[/]]..temp_destination_number..[["/>]]);
+	end
+    return xml
+end
